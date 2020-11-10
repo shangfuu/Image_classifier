dd55f63c6e26d878a12e97f6dc1bacde380f89b7
shungfu <jordan880805@gmail.com>
2020-11-11 00:16:34 +0800

    app.py save images

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Classify photos of Dogs and Cats.
 
 > Dataset: https://www.microsoft.com/en-us/download/details.aspx?id=54765
-## Data prepare
+## Data
+* Organise the dataset as follows:
 ```
 Data
     pickles
@@ -18,3 +19,17 @@ PetImages
     Dog
         (images)
 ```
+## Install
+* Type <code>pip install -r requirements.txt</code> to install all packages.
+* Please check config.py for following steps.
+
+## Prepare dataset
+Type <code> python prepare.py</code> to make pickle files for your PetImages.
+## Training
+Type <code>python train.py </code> to train simple model/
+Type <code>python train.py -m hard</code> to train model with more accuracy one.
+## Testing
+Type <code>python app.py</code> to test your cat / dog images in testData folder.
+
+<img src="/result/cat1.jpeg">
+<img src="/result/dog1.jpg">
--- app.py
+++ app.py
@@ -8,23 +8,30 @@ from config import Categories, test_path, model_name
 
 model = tf.keras.models.load_model(model_name)
 
-for img in os.listdir(test_path):
+for img_name in os.listdir(test_path):
     # Predict Data
     Img_size = 100  # 50 in txt-based
-    img_array = cv2.imread(os.path.join(test_path, img), cv2.IMREAD_GRAYSCALE)
-    if img_array is None:
+    img_gray = cv2.imread(os.path.join(test_path, img_name), cv2.IMREAD_GRAYSCALE)
+    if img_gray is None:
         continue
-    new_array = cv2.resize(img_array, (Img_size, Img_size))
-    new_array = new_array.reshape(-1, Img_size, Img_size, 1)
+    img_gray = cv2.resize(img_gray, (Img_size, Img_size))
+    img_gray = img_gray.reshape(-1, Img_size, Img_size, 1)
     # prediction
-    prediction = model.predict(new_array)
+    prediction = model.predict(img_gray)
 
     # show image
-    img = cv2.imread(os.path.join(test_path, img))
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    plt.imshow(img)
-    # Not showing Coordinate size
+    img = cv2.imread(os.path.join(test_path, img_name))
+    img_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    title = 'I Guess I Am a %s ' % Categories[int(prediction[0][0])]
+    plt.imshow(img_RGB)
     plt.axis('off')
-    plt.title('I Guess I Am a %s ' % Categories[int(prediction[0][0])])
+    plt.title(title)
     plt.show()
 
+    cv2.putText(img, title, (15, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0))
+    cv2.imshow(title, img)
+    if not os.path.exists('result'):
+        os.makedirs('result')
+    cv2.imwrite(os.path.join('result', img_name), img)
+    cv2.waitKey(0)
+    
